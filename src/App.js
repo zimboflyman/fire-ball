73729536bcd5f6dcd11141e325bcc17ace322b8b
zimboflyman <6759437+zimboflyman@users.noreply.github.com>
2022-09-09 20:41:01 +0100

    get data - improve with fetch instead

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect, useErrorHandler } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 import "./App.css";
 
 const App = () => {
+  // get the api data from https://ssd-api.jpl.nasa.gov/doc/fireball.html
+  const apiUrl = "https://ssd-api.jpl.nasa.gov/fireball.api";
+
+  // const handleError = useErrorHandler();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const [data, setData] = useState([]);
+
+  // use fetch instead of try / catch
+  // const callAPI = async () => {
+  //   const response = await fetch(apiUrl);
+  //   const jsonData = await response.json();
+  //   setData(jsonData);
+  // };
+  // useEffect(() => {
+  // (async function () {
+  //   await callAPI();
+  // })();
+  // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        console.log("response Status = ", response.status);
+        if (!response.ok) {
+          setIsError(true);
+          console.log("response not ok Status = ", response.status);
+          // (error) => handleError(error);
+          // throw Error(response.status);
+        }
+        const jsonData = await response.json();
+        console.log("jsonData:::", jsonData);
+        setData(jsonData);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("error here!", error.message);
+        setIsLoading(false);
+        console.log("isError = ", isError);
+        setIsError(true);
+      }
+    };
+    fetchData();
+  }, [apiUrl, isError]);
+
+  const ErrorFallback = ({ error, resetErrorBoundary }) => {
+    return (
+      <div role="alert">
+        <p>Something went wrong while fetching fireBall data:</p>
+        <pre>{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => {
+            // reset the state of your app so the error doesn't happen again
+          }}
         >
-          Learn React
-        </a>
+          <p>{`returned: ${data.count} records`} .</p>
+        </ErrorBoundary>
       </header>
     </div>
   );
