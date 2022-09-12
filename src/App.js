3918a7c2c281fe1d3d643c339a4a77f3ad098841
zimboflyman <6759437+zimboflyman@users.noreply.github.com>
2022-09-12 07:48:50 +0100

    flight data experiment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { useState, useEffect, useErrorHandler } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useErrorHandler,
+} from "react";
 import { ErrorBoundary } from "react-error-boundary";
+
+import "echarts-gl";
+import ReactEcharts from "echarts-for-react";
+import * as echarts from "echarts";
 
 import "./App.css";
 
 const App = () => {
   // get the api data from https://ssd-api.jpl.nasa.gov/doc/fireball.html
-  const apiUrl = "https://ssd-api.jpl.nasa.gov/fireball.api";
+  // const apiUrl = "https://ssd-api.jpl.nasa.gov/fireball.api";
+  // const apiUrl =
+  //   "https://echarts.apache.org/examples/data-gl/asset/data/population.json";
+  const apiUrl =
+    "https://echarts.apache.org/examples/data-gl/asset/data/flights.json";
+
+  const ref = useRef(null);
 
   // const handleError = useErrorHandler();
 
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [data, setData] = useState();
+  let option;
 
   // use fetch instead of try / catch
   // const callAPI = async () => {
@@ -26,15 +42,170 @@ const App = () => {
   // })();
   // }, []);
 
+  // const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  //   return (
+  //     <div role="alert">
+  //       <p>Something went wrong while fetching fireBall data:</p>
+  //       <pre>{error.message}</pre>
+  //       <button onClick={resetErrorBoundary}>Try again</button>
+  //     </div>
+  //   );
+  // };
+
+  // todo list -
+  // download assets
+  // globeChart.resize
+  // speedup rotate
+  // get minimal bundle
+  const ROOT_PATH = "https://echarts.apache.org/examples";
+
+  // data from api
+  // 0: "date"
+  // 1: "energy"
+  // 2: "impact-e"
+  // 3: "lat"
+  // 4: "lat-dir"
+  // 5: "lon"
+  // 6: "lon-dir"
+  // 7: "alt"
+  // 8: "vel"
+
+  // .filter((dataItem) => dataItem[2] > 4)
+  // .map((dataItem) => [dataItem[3], dataItem[5], dataItem[7]]);
+
+  // const globeData = data
+  //   .filter((dataItem) => dataItem[2] > 4)
+  //   .map((dataItem) => [dataItem[0], dataItem[1], Math.sqrt(dataItem[2])]);
+
+  // const getCometOptions = useCallback(() => {
+  //   const globeData = data
+  //     // ?.filter((dataItem) => dataItem[2] > 0)
+  //     ?.map((dataItem) => [dataItem[3], dataItem[5], dataItem[7]]);
+
+  //   console.log("globeData inside getChartOptions:", globeData);
+
+  //   option = {
+  //     backgroundColor: "#000",
+  //     globe: {
+  //       // displacementScale: 5,
+  //       baseTexture: ROOT_PATH + "/data-gl/asset/world.topo.bathy.200401.jpg",
+  //       heightTexture: ROOT_PATH + "/data-gl/asset/world.topo.bathy.200401.jpg",
+  //       // heightTexture:
+  //       //   ROOT_PATH + "/data-gl/asset/bathymetry_bw_composite_4k.jpg",
+
+  //       shading: "lambert",
+  //       environment: ROOT_PATH + "/data-gl/asset/starfield.jpg",
+  //       light: {
+  //         ambient: {
+  //           intensity: 0.4,
+  //         },
+  //         main: {
+  //           intensity: 0.4,
+  //         },
+  //       },
+  //       // globeRadius: 100,
+  //       globeOuterRadius: 300,
+  //       viewControl: {
+  //         maxDistance: 1500,
+  //         zoomSensitivity: 5,
+  //         panSensitivity: 5,
+  //         autoRotate: false,
+  //       },
+  //     },
+  //     visualMap: {
+  //       max: 40,
+  //       calculable: true,
+  //       realtime: false,
+  //       inRange: {
+  //         colorLightness: [0.2, 0.9],
+  //       },
+  //       textStyle: {
+  //         color: "#fff",
+  //       },
+  //       controller: {
+  //         inRange: {
+  //           color: "orange",
+  //         },
+  //       },
+  //       outOfRange: {
+  //         colorAlpha: 0,
+  //       },
+  //     },
+  //     series: [
+  //       {
+  //         type: "bar3D",
+  //         coordinateSystem: "globe",
+  //         data: globeData,
+  //         barSize: 1,
+  //         minHeight: 0.2,
+  //         silent: false,
+  //         itemStyle: {
+  //           color: "orange",
+  //         },
+  //       },
+  //     ],
+  //   };
+  // });
+
+  // useCallback hook - used to maintain the same memory reference through each render to prevent endless rendering—unless something in its dependency array changes
+  const getChartOptions = useCallback(() => {
+    function getAirportCoord(idx) {
+      return [data.airports[idx][3], data.airports[idx][4]];
+    }
+
+    console.log("data inside getChartOptions:", data);
+
+    const routes = data?.routes?.map(function (airline) {
+      return [getAirportCoord(airline[1]), getAirportCoord(airline[2])];
+    });
+    option = {
+      backgroundColor: "#000",
+      globe: {
+        baseTexture: ROOT_PATH + "/data-gl/asset/world.topo.bathy.200401.jpg",
+        heightTexture:
+          ROOT_PATH + "/data-gl/asset/bathymetry_bw_composite_4k.jpg",
+        shading: "lambert",
+        light: {
+          ambient: {
+            intensity: 0.4,
+          },
+          main: {
+            intensity: 0.4,
+          },
+        },
+        viewControl: {
+          autoRotate: false,
+        },
+      },
+      series: {
+        type: "lines3D",
+        coordinateSystem: "globe",
+        blendMode: "lighter",
+        lineStyle: {
+          width: 1,
+          color: "rgb(50, 50, 150)",
+          opacity: 0.1,
+        },
+        data: routes,
+      },
+    };
+    // const chartDom = ref.current;
+    // const globeChart = echarts.init(chartDom);
+
+    // option && globeChart.setOption(option);
+  });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(apiUrl);
         console.log("response Status = ", response.status);
         if (!response.ok) {
-          setIsError(true);
+          setHasError(true);
+          setIsLoading(false);
+
           console.log("response not ok Status = ", response.status);
-          // (error) => handleError(error);
+          // error => handleError(error);
           // throw Error(response.status);
         }
         const jsonData = await response.json();
@@ -44,26 +215,34 @@ const App = () => {
       } catch (error) {
         console.log("error here!", error.message);
         setIsLoading(false);
-        console.log("isError = ", isError);
-        setIsError(true);
+        console.log("isError = ", hasError);
+        setHasError(true);
       }
     };
     fetchData();
-  }, [apiUrl, isError]);
+    console.log("data", data);
+  }, []);
 
-  const ErrorFallback = ({ error, resetErrorBoundary }) => {
-    return (
-      <div role="alert">
-        <p>Something went wrong while fetching fireBall data:</p>
-        <pre>{error.message}</pre>
-        <button onClick={resetErrorBoundary}>Try again</button>
-      </div>
-    );
-  };
+  useEffect(() => {
+    data && getChartOptions();
+    const chartDom = ref.current;
+    const globeChart = echarts.init(chartDom);
+
+    option && globeChart.setOption(option);
+
+    return () => {};
+  }, [data, getChartOptions, option]);
 
   return (
     <div className="App">
-      <header className="App-header">
+      {isLoading && <div className="loader">Loading</div>}
+      {hasError && <div className="loader">hasError!!!</div>}
+
+      <div ref={ref} style={{ width: "100vw", height: "100vh" }} />
+      {/* {data?.length > 0 && getChartOptions(data)} */}
+
+      {/* <ReactEcharts option={option} /> */}
+      {/* <header className="App-header">
         <ErrorBoundary
           FallbackComponent={ErrorFallback}
           onReset={() => {
@@ -72,7 +251,7 @@ const App = () => {
         >
           <p>{`returned: ${data.count} records`} .</p>
         </ErrorBoundary>
-      </header>
+      </header> */}
     </div>
   );
 };
